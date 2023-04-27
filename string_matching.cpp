ae8fa7eaeea3d012b45f81a110918d4ab5b8baf1
Vigneswar-A <ugiv.rawsengiv@gmail.com>
2023-04-27 20:52:54 +0530

    added rabinkarp

--- string_matching.cpp
+++ string_matching.cpp
@@ -18,8 +18,46 @@ void naive_string_matching(string text, string pattern)
     }
 }
 
+void rabin_karp(string text, string pattern, int q)
+{
+    int d = 26;
+    int m = text.length(), n = pattern.length();
+    int h = 1;
+    int pattern_hash = 0;
+    int text_hash = 0;
+
+    for (int i = 0; i < n - 1; i++)
+        h = (h * d) % q;
+
+    for (int i = 0; i < n; i++)
+    {
+        pattern_hash = (pattern_hash * d + pattern[i]) % q;
+        text_hash = (text_hash * d + text[i]) % q;
+    }
+
+    for (int i = 0; i < m - n + 1; i++)
+    {
+        if (text_hash == pattern_hash)
+        {
+            bool flag = true;
+            for (int j = 0; j < n && flag; j++)
+                if (pattern[j] != text[i + j])
+                    flag = false;
+            if (flag)
+                cout << "pattern matched at " << i << endl;
+        }
+        if (i < m - n)
+        {
+            text_hash = ((text_hash - (h * text[i])) * d + text[i + n]) % q;
+            if (text_hash < 0)
+                text_hash += q;
+        }
+    }
+}
+
 int main(void)
 {
-    naive_string_matching("abcabcababac", "babac");
+    // naive_string_matching("abcabcababac", "babac");
+    rabin_karp("abdabcabacdbabac", "aba", 17);
     return 0;
 }
